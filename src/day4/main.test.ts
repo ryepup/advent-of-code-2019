0493114927603bf2bd958aef4309c7638e35d20d
Ryan Davis <ryepup@gmail.com>
2019-12-07 13:32:07 -0500

    Day 4, part 2
    I wish iterables had more methods on them. Feels wasteful
    to `[...iterable ]` when I might not need to process
    the entire iterable.

--- src/day4/main.test.ts
+++ src/day4/main.test.ts
@@ -1,6 +1,10 @@
-import { countPasswords } from './main'
+import { countPasswords, hasSequentialDigits, hasTwoSequentialDigits } from './main'
 describe('day4', () => {
     describe('part1', () => {
-        expect(countPasswords()).toBe(1019);
+        expect(countPasswords(hasSequentialDigits)).toBe(1019);
+    })
+
+    describe('part2', () => {
+        expect(countPasswords(hasTwoSequentialDigits)).toBe(660);
     })
 })
--- src/day4/main.ts
+++ src/day4/main.ts
@@ -2,32 +2,43 @@
 const min = 248345
 const max = 746315
 
-export function countPasswords() {
+export function countPasswords(predicate: ((input: number) => boolean)) {
     return [...generatePassword(
+        predicate,
         0,
         parseInt(min.toString()[0]),
-        parseInt(max.toString()[0]),
+        parseInt(max.toString()[0])
     )].length
 }
 
-function* generatePassword(input: number, min: number, max = 9)
+function* generatePassword(predicate: ((input: number) => boolean), input: number, min: number, max = 9)
     : Generator<number, any, undefined> {
     for (let i = min; i <= max; i++) {
         const next = input * 10 + i;
         if (next > 100000) {
-            if (isValid(next)) { yield next; }
+            if (isValid(next) && predicate(next)) { yield next; }
         }
-        else { yield* [...generatePassword(next, i)]; }
+        else { yield* [...generatePassword(predicate, next, i)]; }
     }
 }
 
 function isValid(input: number): boolean {
     return input > min
         && input < max
-        && hasSequentialDigits(input)
 }
 
-function hasSequentialDigits(input: number): boolean {
+export function hasSequentialDigits(input: number): boolean {
     const digits = input.toString().split('');
     return new Set(digits).size < digits.length;
+}
+
+
+export function hasTwoSequentialDigits(input: number): boolean {
+    const digits = input.toString().split('');
+    const counts = digits.reduce((map, d) => {
+        const n = 1 + (map.get(d) || 0);
+        return map.set(d, n);
+    }, new Map<string, number>())
+    return [...counts.values()]
+        .includes(2);
 }
